9451ce20ee22bcf8758bebf576b48e70a71512b2
moamine-max <arafiesta1tani@gmail.com>
2024-01-26 16:15:10 +0100

    fixed

--- callmouhami/settings.py
+++ callmouhami/settings.py
@@ -128,7 +128,7 @@ DATABASES = {
         'NAME': 'mouhamidb',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': '127.0.0.1',  
+        'HOST': 'localhost',  
         'PORT': '3306',  
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
